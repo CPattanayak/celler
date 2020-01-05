2ef99d21cade0adb43485b851d785995629c59f6
pattac <pattac@INGSPATTACL2C>
2020-01-05 11:10:33 +0530

    added deployment script to wait unless pod is up

--- depl-sh.sh
+++ depl-sh.sh
@@ -2,8 +2,7 @@
 #
 # Script to download and deploy and openwhisk actions
 #
-sed 's/<imageid>/$BUILD_NUMBER/g' deployment.yaml > deploy.yaml
-kubectl apply -f deploy.yaml
+
 
 COUNT=$(kubectl get pods | grep -c celler-deployment)
 while [[ "$COUNT" != 1 ]]; do
