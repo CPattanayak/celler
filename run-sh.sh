c713234a85080c1dfe49032a5b4aeca0590dce01
pattac <pattac@INGSPATTACL2C>
2020-02-21 19:07:20 +0530

    allure report

--- run-sh.sh
+++ run-sh.sh
@@ -5,11 +5,11 @@
 
 behave -f allure_behave.formatter:AllureFormatter -o allure-results behaveTest/features/feature/*.feature
 if [[ $? -ne 0 ]]; then
-    ls -lrt allure-results
+   # ls -lrt allure-results
     allure generate allure-results -o allure-report
-	ls -lrt allure-report
-	tar -zcvf allure-report.tar.gz -C allure-report 
+	#ls -lrt allure-report
+	tar -zcvf allure-report.tar.gz allure-report 
     exit 1
 fi
 allure generate allure-results -o allure-report
-tar -zcvf allure-report.tar.gz -C allure-report .
+tar -zcvf allure-report.tar.gz  allure-report .
